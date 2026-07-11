dcb2cba916d7b9a9fa0c2ac4dadb569f050cecd3
Lukas Kirner <l.t.kirner@tue.nl>
2026-07-11 22:11:48 +0200

    Remove underline from Repo button in cite-widget
    - Add text-decoration:none to .cite-btn (removes default <a> underline)
    - Switch brand logo to icon-1024.png
    - Add JetBrains Mono font imports

--- web/wasm-prototype/src/entry.js
+++ web/wasm-prototype/src/entry.js
@@ -1,3 +1,8 @@
+import "@fontsource/jetbrains-mono/400.css";
+import "@fontsource/jetbrains-mono/500.css";
+import "@fontsource/jetbrains-mono/600.css";
+import "@fontsource/jetbrains-mono/700.css";
+
 import "./pipeline/app.js";
 
 // Orientation overlay for phones
